b602acb03cd7e1f0139f865a0f5cb1cdfe7aadbb
Rafael Silveira <rafael.silveira@fullsoft.com.br>
2018-05-29 14:03:49 -0300

    criacao inicial do projeto

---
+++ Soundenizer/Soundenizer/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.SimpleAudioPlayer;
+using Xamarin.Forms;
+
+namespace Soundenizer
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+	    private void BtnSound_OnClicked(object sender, EventArgs e)
+	    {
+	        Button b = (Button) sender;
+
+	        ISimpleAudioPlayer player = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.Current;
+	        player.Load("sound" + b.Text + ".mp3");
+	        player.Play();
+	    }
+	}
+}
